466abffcf51e5f3bab82d2963a0f7bf84113502d
Kasyanov Maxim <gitsby@outlook.com>
2019-10-17 23:48:54 +0600

    add test for where clause

--- src/test/java/com/github/gitsby/sql/SQLSelectTest.java
+++ src/test/java/com/github/gitsby/sql/SQLSelectTest.java
@@ -8,12 +8,56 @@ import org.junit.Test;
 public class SQLSelectTest {
 
   @Test
-  public void select_from() {
+  public void from() {
     SQL sql = new SQL();
     sql.select("1");
     sql.from("test_table");
-    String s = sql.toString();
-    assertThat(s).isEqualToIgnoringCase("select 1\nfrom test_table");
+    String sqlQuery = sql.toString();
+    assertThat(sqlQuery).isEqualToIgnoringCase("select 1\nfrom test_table");
   }
+
+  @Test
+  public void select() {
+    SQL sql = new SQL();
+    sql.select("1!");
+    String sqlQuery = sql.toString();
+    assertThat(sqlQuery).isEqualToIgnoringCase("select 1!");
+  }
+
+
+  @Test
+  public void select_multiple_columns() {
+    SQL sql = new SQL();
+    sql.select("1!");
+    sql.select("2");
+    sql.select("3");
+    String sqlQuery = sql.toString();
+    assertThat(sqlQuery).isEqualToIgnoringCase("select 1!, 2, 3");
+  }
+
+
+  @Test
+  public void where() {
+    SQL sql = new SQL();
+    sql.select("1");
+    sql.select("2");
+    sql.from("test_table");
+    sql.where("test_table.column1 = 'asd'");
+    String sqlQuery = sql.toString();
+    assertThat(sqlQuery).isEqualToIgnoringCase("select 1, 2\nfrom test_table\nwhere ( test_table.column1 = 'asd' )");
+  }
+
+  @Test
+  public void where_multiple() {
+    SQL sql = new SQL();
+    sql.select("1");
+    sql.select("2");
+    sql.from("test_table");
+    sql.where("test_table.column1 = 'asd'");
+    sql.where("test_table.column2 >= 42");
+    String sqlQuery = sql.toString();
+    assertThat(sqlQuery).isEqualToIgnoringCase("select 1, 2\nfrom test_table\nwhere test_table.column1 = 'asd' and test_table.column2 >= 42 ");
+  }
+
 
 }
